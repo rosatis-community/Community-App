8995047289fcbf668a4f124b8a58b4a288204b7d
rosatis-community <rosatis.community@gmail.com>
2021-08-22 23:15:19 +0300

    Update express.js

--- express.js
+++ express.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const { resolve } = require('path');
 const app = express();
 const portNumber = 3000;
 const sourceDir = 'dist';
 
 app.use(express.static(sourceDir));
 
-app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, 'index.html.ejs'))
+app.get('/*', (request, response) => {
+  response.sendFile(resolve(__dirname, sourceDir, 'index.html'))
 });
 
 app.listen(portNumber, () => {
