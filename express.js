3a27f494717aa3e10b577c37b874cb200ff0196e
rosatis-community <rosatis.community@gmail.com>
2021-08-22 22:50:49 +0300

    Update express.js

--- express.js
+++ express.js
@@ -5,6 +5,10 @@ const sourceDir = 'dist';
 
 app.use(express.static(sourceDir));
 
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'index.html.ejs'))
+});
+
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
   console.log(`Serving content from /${sourceDir}/`);
