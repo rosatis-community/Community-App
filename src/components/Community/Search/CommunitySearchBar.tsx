367151a173cb19e5c728e943b09fd3e68dad74de
rosatis-community <rosatis.community@gmail.com>
2021-08-22 11:03:19 +0300

    Search bug fixed

--- src/components/Community/Search/CommunitySearchBar.tsx
+++ src/components/Community/Search/CommunitySearchBar.tsx
@@ -9,21 +9,22 @@ import SearchPopper from './SearchPopper';
 
 const CommunitySearchBar = () => {
   const searchBarRef = useRef<HTMLDivElement>(null);
+  const [anchorEl, setAnchorEl] = useState(null);
   const [term, setTerm] = useState('');
   const debouncedValue = useDebounce(term, 500);
+
+  const onCompleted = (data: CommunitiesData) => {
+    setAnchorEl(searchBarRef.current);
+  }
+
   const [lazyQuery, { loading, error, data }] =
-    useLazyQuery<CommunitiesData>(SEARCH_COMMUNITIES);
-  const [anchorEl, setAnchorEl] = useState(null);
+    useLazyQuery<CommunitiesData>(SEARCH_COMMUNITIES, { onCompleted });
+
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTerm(event.target.value);
   };
 
-  useEffect(() => {
-    if (data && data.searchCommunities?.length) {
-      setAnchorEl(searchBarRef.current);
-    }
-  }, [data]);
 
   useEffect(() => {
     if (debouncedValue) {
@@ -36,7 +37,7 @@ const CommunitySearchBar = () => {
       <div ref={searchBarRef}>
         <CSearchBar term={term} handleInputChange={handleInputChange} />
       </div>
-      <SearchPopper open={!!anchorEl} anchorEl={anchorEl} onClickAway={() => setAnchorEl(null)} placement="bottom-start">
+      <SearchPopper open={Boolean(anchorEl)} anchorEl={anchorEl} onClickAway={() => setAnchorEl(null)} placement="bottom-start">
         {data && <SearchList communities={data.searchCommunities} />}
       </SearchPopper>
     </div>
