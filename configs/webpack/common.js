7d209089de290058aa6cb61c6ba2e4014c1f47a7
rosatis-community <rosatis.community@gmail.com>
2021-08-22 23:56:56 +0300

    favicon

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -32,7 +32,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "index.html.ejs", publicPath: '/', favicon: '../../src/assets/img/favicon.ico' }),
+    new HtmlWebpackPlugin({ template: "index.html.ejs", publicPath: '/', favicon: 'favicon.ico' }),
   ],
   externals: {
     react: "React",
